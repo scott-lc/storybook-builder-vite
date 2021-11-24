693464809952c5c7595ae942b5cc61b5da1ce1d7
Scott Sadler <scott@livecontrol.io>
2021-11-24 13:31:14 -0500

    Potential fix for issue #164

--- packages/storybook-builder-vite/source-loader-plugin.js
+++ packages/storybook-builder-vite/source-loader-plugin.js
@@ -1,13 +1,24 @@
 const inject =
     require('@storybook/source-loader/dist/cjs/abstract-syntax-tree/inject-decorator').default;
 
+const { sanitizeSource } = require('@storybook/source-loader/dist/cjs/abstract-syntax-tree/generate-helpers');
+
 module.exports.sourceLoaderPlugin = function () {
     return {
         name: 'storybook-vite-source-loader-plugin',
         enforce: 'pre',
         transform(src, id) {
             if (id.match(/\.stories\.[jt]sx?$/)) {
-                const { source, sourceJson, addsMap } = inject(src, id);
+                const result = inject(src, id);
+
+                const sourceJson = sanitizeSource(result.storySource || src);
+                const addsMap = result.addsMap || {};
+                const source = result.source;
+
+                if (!source || source.length === 0) {
+                  return src;
+                }
+
                 const preamble = `
                     /* eslint-disable */
                     // @ts-nocheck
